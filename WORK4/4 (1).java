11bf7992e6b95864c3b7ed13b07f5071f1e7fbe8
djt6668 <71829455+djt6668@users.noreply.github.com>
2022-01-04 00:22:06 +0800

    Add files via upload

---
+++ WORK4/4 (1).java
@@ -0,0 +1,16 @@
+package Ex4;
+
+public class TeseCircleWithStaticMembers {
+	public static void main(String[] args) {
+		System.out.println("Before creating objects");
+		System.out.println("The number of Circle objects is " + Circle2.numberOfObjects);
+		
+		Circle2 c1 = new Circle2();
+		System.out.println("\nAfter creating c1");
+		System.out.println("c1: radius (" + c1.radius + c1.numberOfObjects + ")");
+		
+		Circle2 c2 = new Circle2();
+		System.out.println("\nAfter creating c2");
+		System.out.println("c2: radius (" + c2.radius + c2.numberOfObjects + ")");
+	}
+}
